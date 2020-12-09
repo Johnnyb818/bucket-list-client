fa1220da07ed7a46d1b6fa077c1894a008bbff15
John Bruno <johnnyb@MacBook-Pro.local>
2020-12-09 13:33:15 -0600

    AppState setup

---
+++ src/AppState.jsx
@@ -0,0 +1,46 @@
+import React, { useReducer } from "react"
+
+///////////////////////////
+//Initial State
+//////////////////////////
+
+const initialState = {
+    url: "http://jb-bucket-list-api.herokuapp.com"
+}
+
+
+//////////////////////
+//Reducer
+/////////////////////
+// action = {type: "", payload: ---}
+const reducer = (state, action) => {
+    
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+//////////////////////
+//AppContext
+//////////////////////
+const AppContext = React.createContext(null)
+
+
+////////////////////
+//AppState Component
+/////////////////////
+export const AppState = (props) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return <AppContext.Provider value={{state, dispatch}}>{props.children}</AppContext.Provider> 
+};
+
+/////////////////////
+//useAppState hook
+///////////////////
+
+export const useAppState = () => {
+    return React.useContext(AppContext)
+}
+
